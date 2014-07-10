7204107ecbfda2df4aa40598f4b7c32aba2301c6
Laurent Lasudry <lasudry@gmail.com>
2014-07-10 15:16:02 +0200

    Initial package structure

---
+++ collective/directory/testing.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from plone.testing import z2
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneWithPackageLayer
+from plone.app.robotframework.testing import AUTOLOGIN_LIBRARY_FIXTURE
+
+import collective.directory
+
+from plone.app.testing import applyProfile
+
+
+class CollectiveDirectoryPloneWithPackageLayer(PloneWithPackageLayer):
+    """
+    """
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'collective.directory:testing')
+        footer_static = portal['footer-static']
+        footer_static.setText('Footer static custom content')
+
+
+COLLECTIVE_DIRECTORY_FIXTURE = CollectiveDirectoryPloneWithPackageLayer(
+    name="COLLECTIVE_DIRECTORY_FIXTURE",
+    zcml_filename="testing.zcml",
+    zcml_package=collective.directory,
+    gs_profile_id="collective.directory:testing")
+
+COLLECTIVE_DIRECTORY_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(COLLECTIVE_DIRECTORY_FIXTURE,),
+    name="collective.directory:Integration")
+
+COLLECTIVE_DIRECTORY_ROBOT_TESTING = FunctionalTesting(
+    bases=(COLLECTIVE_DIRECTORY_FIXTURE, AUTOLOGIN_LIBRARY_FIXTURE,
+           z2.ZSERVER_FIXTURE),
+    name="collective.directory:Robot")
