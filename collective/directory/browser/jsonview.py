41e7c111b8cd98e2ab4259fc69a6d0c028a968a3
boulch <christophe.boulanger@imio.be>
2015-03-24 15:02:51 +0100

    If a card is in another card, obj.aq_parent.portal_type is a card so, cat ID is not a key in self.context and that crash the data_categories in map_directories.js

--- collective/directory/browser/jsonview.py
+++ collective/directory/browser/jsonview.py
@@ -25,6 +25,8 @@ class JsonDirectory(BrowserView):
     def get_brain(self):
         querydict = {}
         querydict['path'] = {'query': '/'.join(self.context.getPhysicalPath())}
+        #querydict['portal_type'] = 'collective.directory.category'
+        #querydict['depth'] = 1
         return self.portal_catalog(querydict)
 
     def get_categories(self):
@@ -33,25 +35,33 @@ class JsonDirectory(BrowserView):
             if brain.zgeo_geometry:
                 marker = {}
                 cat = brain.collective_directory_category
+                isaCard = 'collective.direcotry.category'
                 if not cat:
                     obj = brain.getObject()
                     cat = obj.aq_parent.id
+                    # Be Careful : If a card is in another card, obj.aq_parent.portal_type is a card so, cat ID is not a key in self.context
+                    isaCard = obj.aq_parent.portal_type
                     logger.info("Obj: {0} not good for performance, start collective.directory upgradesteps 2".format(obj.id))
-                geom = {'type': brain.zgeo_geometry['type'],
-                        'coordinates': brain.zgeo_geometry['coordinates']}
-                marker['geom'] = geom
-                marker['style'] = brain.collective_geo_styles
-                marker['title'] = brain.Title
-                marker['description'] = brain.Description
-                marker['category'] = cat
-                marker['url'] = brain.getURL()
-                if cat not in categories.keys():
-                    categories[cat] = {}
-                    categories[cat]['title'] = self.context[cat].title
-                    img_url = get_marker_image(
-                        self.context,
-                        marker['style'].get('marker_image'))
-                    categories[cat]['img_url'] = img_url
-                    categories[cat]['markers'] = []
-                categories[cat]['markers'].append(marker)
+                if isaCard == 'collective.directory.category':
+                    geom = {'type': brain.zgeo_geometry['type'],
+                            'coordinates': brain.zgeo_geometry['coordinates']}
+                    marker['geom'] = geom
+                    marker['style'] = brain.collective_geo_styles
+                    marker['title'] = brain.Title
+                    marker['description'] = brain.Description
+                    marker['category'] = cat
+                    marker['url'] = brain.getURL()
+                    # Attention plante quand sous carte dans carte.
+                    # if "lafforgue-jean-philippe" in cat:
+                    #    import ipdb
+                    #    ipdb.set_trace()
+                    if cat not in categories.keys():
+                        categories[cat] = {}
+                        categories[cat]['title'] = self.context[cat].title
+                        img_url = get_marker_image(
+                            self.context,
+                            marker['style'].get('marker_image'))
+                        categories[cat]['img_url'] = img_url
+                        categories[cat]['markers'] = []
+                    categories[cat]['markers'].append(marker)
         return "var data_categories = {0}".format(json.dumps(categories, sort_keys=True))
